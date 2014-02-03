4315bcc96f0b9e5bef4fd2da78718a35fe05809a
Justin Crowell & Mark Protas <mprotas@truecar.com>
2014-02-02 22:54:28 -0600

    Cleaned up css parse errors, moved image nav into the unordered list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 $(function(){
-      $('#Grid').mixitup();
-      $('#Grid').hide();
-      $(".button.filter").click($('#Grid').show());
-      $(".sticky-element").sticky({ topSpacing: 0});
+	// initialize mixitup
+	$('#Grid').mixitup({
+		resizeContainer: false,
+		showOnLoad: 'nav'
+	});
+	// only display the wheels on load
+	//$('div[data-filter=wheel]').click();
+    $(".sticky-element").sticky({ topSpacing: 0});
 });
