3cbfbfd391dba4eab76c661c13ec564290ce626a
killboxsarah <killbox.sarah@gmail.com>
2014-11-21 17:10:34 -0600

    updated to remove extension

--- wheel.php
+++ wheel.php
@@ -17,7 +17,7 @@ $product = $products[$product_id];
     <div class="large-12 small-12 columns">
 	<div class ="panel landings full-height" style="opacity:1;">
 		<div class="section page">
-			<div class="breadcrumb"><a href="index.php" style="color:#662e91;">Faster</a> &gt; <a href="wheels.php" style="color:#86c43f;">Wheels</a> &gt; <?php echo $product["name"]; ?></div>
+			<div class="breadcrumb"><a href="index" style="color:#662e91;">Faster</a> &gt; <a href="wheels" style="color:#86c43f;">Wheels</a> &gt; <?php echo $product["name"]; ?></div>
 				<br>
 
 					<div class = "large-6 small-12 columns">
