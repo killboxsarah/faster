fbe66a9e4804b27a4fa57f230c3399b764421f31
killboxsarah <killbox.sarah@gmail.com>
2015-06-02 16:40:48 -0500

    updating for flywheel release

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $section = "home";
  <p class="detail" style="color: #808285;">TRUE TO THE <span class="alt">ART</span> </p>
 
   <div class="large-12 small-12">
-    <center><h5 style="color: whitesmoke;"> Faster is driven to engineer and create roller skating products designed to stand the test of time, providing the performance advantage derived from the highest quality equipment. </br>
+    <center><h5 style="color: whitesmoke;max-width: 80%;"> Faster is driven to engineer and create roller skating products designed to stand the test of time, providing the performance advantage derived from the highest quality equipment. </br>
     </br>
     We love to skate ourselves and know what it feels like to be on wheels that compliment your skills perfectly and reward all the hard work you put in to your skating. We want to maximize your ability to get the best combination of grip, roll, responsiveness and control on any surface.<br>
     <br>
@@ -22,13 +22,34 @@ $section = "home";
 </div>
 
 <hr>
+    <div class ="panel product full-height" style="background-color: #662e91;">
+     <p class="detail" style="color: #161616;">SKATE <span class="name">FASTER</span> </p>
+  
+      <div class="large-12 small-12 columns">
+        <center><h5 style="color: #808285;max-width: 80%;">    
+        Skating is an art form, and skaters deserve gear that remains true to that art. 
+        <br>Combining years of our own skating experience with the finest materials and technologically driven craftsmanship, 
+        we engineer what your hard work deserves - wheels with unparalleled precision, responsiveness and speed. 
+        <br><br>Forget what you think you know. This is Faster.<br>
+        <br></div>
 
-<div class="panel product full-height">
- <p class="detail" >OUR <span class="name">WHEELS</span> </p>
+      <div class="large-6 small-12 columns hide-for-small">
+             <iframe width="440" height="315" src="https://www.youtube.com/embed/lnUpxetTqBU" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+    <div class="large-6 small-12 columns hide-for-small">
+    <iframe width="440" height="315" src="https://www.youtube.com/embed/FLVVbzWZiDU" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+    </div>
+    <hr>
+
+<div class="panel product full-height" style="background-color: #eeeeee;">
+ <p class="detail" style="color:#808285;" >FASTER <span class="name" style="color:#ed1c24;">WHEELS</span> </p>
 
    <div class="large-6 small-12 columns">
           <ul class="panel frame pricing-table left-side">
-            <center><a  href="multicomponents"><img src="images/left.jpg" alt="Single Component"></a></center>
+            <center><a  href="multicomponents"><img src="images/front_page_multi.jpg" alt="Multi Component" style= "max-height:349px"></a></center>
             <li class="price" style="font-size:1.1em; text-aligh:center;">MULTI COMPONENT</li>
             </ul>
           </div>
@@ -36,8 +57,8 @@ $section = "home";
 
     <div class="large-6 small-12 columns">
           <ul class="panel frame pricing-table left-side john">
-             <center><a href="singlecomponents"><img src="images/right.jpg" alt="Single Component" style="max-height:349px;"></a></center>
-            <li class="price" style="font-size:1.1em; text-aligh:center;">SINGLE COMPONENT</li>
+             <center><!-- a href="FlyWheel" --><img src="images/FlyWheel.jpg" alt="Single Component" style="max-height:349px;"></a></center>
+            <li class="price" style="font-size:1.1em; text-aligh:center;">FlyWheel - COMING SOON</li>
             </ul>
           </div>
       
