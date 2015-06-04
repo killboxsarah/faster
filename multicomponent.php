29a03b76b8c01cae685c029b97d9e61de9864eb2
killboxsarah <killbox.sarah@gmail.com>
2015-06-04 18:33:32 -0500

    updaing marks commits

--- multicomponent.php
+++ multicomponent.php
@@ -17,8 +17,8 @@ $product = $dualproducts[$product_id];
     <div class="large-12 small-12 columns">
 	<div class ="panel landings full-height" style="opacity:1;">
 		<div class="section page">
-			<div class="breadcrumb"><a href="index" style="color:#662e91;">Faster</a> &gt; <a href="wheels" style="color:#86c43f;">Wheels</a> &gt; <?php echo $product["name"]; ?></div>
-				<br>
+			<div class="breadcrumb"><a href="index" style="color:#662e91;">Faster</a> <!-- &gt; <a href="wheels" style="color:#86c43f;">Wheels</a> &gt; <?php echo $dualproducts["name"]; ?></div>
+				 --><br><br>
 
 					<div class = "large-6 small-12 columns">
 					<ul class="panel frame pricing-table left-side">
@@ -32,7 +32,6 @@ $product = $dualproducts[$product_id];
 									<h3><?php echo $product["name"]; ?></h3>
 					            </div>
 					        </li>
-					  <!--       <li class="price">$<?php echo $product["price"]; ?> USD -->
 					        </li>
 					        <li class="description"><?php echo $product["details"]; ?>
 					        <br>
