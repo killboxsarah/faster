48b1ba5960f75d4a59f1372f87fe44197151798a
killboxsarah <killbox.sarah@gmail.com>
2015-11-23 15:18:35 -0600

    updating for sponsorhip page

---
+++ hipel.php
@@ -0,0 +1,114 @@
+<?php 
+$pagetitle = "Sarah HIpel"; 
+include('inc/header.php'); 
+$section = "Sponsorship";
+?>
+
+
+<div class="row">
+<div class="large-12 columns">
+<div class="landing">
+  <img src="images/sarah_final.jpg" >  
+  </div>
+
+    <div class ="panel product full-height" style="opacity:.88;">
+     <p class="detail" style="color: #808285;">Sarah <span class="alt">Hipel</span> </p>
+  
+      <div class="large-12 small-12 columns">
+        <center><h5 style="color: #808285;"> 
+        Sarah has continuously held a roster spot on a top 10 team and played in 12 WFTDA Big 5 tournaments, 
+        first with the Detroit Derby Girls then the Texas Rollergirls. Sarah also holds a spot on the Canadian National team, and most recently served as the skating captain of Team Canada. 
+
+        She is one of the most experienced players in the game and continues to develop cross training techniques, coaching, and the skills and strategy of the sport itself. 
+        Sarah has held major roles in the WFDTA and her home leagues.  She also owns and operates Faster Skates.</h5></center>
+      </div>
+    </div>
+</div>
+</div>
+
+<hr>
+<div class="row">
+<div class="large-12 columns">
+<hr>
+    
+    <div class ="panel dealer full-height">
+      <p class="detail" style="color: whitesmoke;">Press<span class="alt"></span> </p>
+
+           <div class="large-12 small-12 columns">
+          <ul class="pricing-table" style="background-color: #fff;">
+           <li class="bullet-item">ESPN</li>
+            <center> <iframe width="900" height="400" src="//espn.go.com/espnw/athletes-life/slideshow/9931745/6/sarah-killbox-hipel" frameborder="0"></iframe></center>
+
+          </ul>
+        </div>
+
+        <div class="large-12 small-12 columns">
+          <ul class="pricing-table" style="background-color: #fff;">
+          <li class="bullet-item">Chicks in Bowls</li>
+           <center><iframe width="900" height="500" src="//www.chicksinbowls.com/derby-vs-ramps/" frameborder="0"></iframe></center>    
+        </ul>
+        </div>
+
+
+    <div class="large-12 small-12 columns">
+          <ul class="pricing-table" style="background-color: #fff;">
+           <li class="bullet-item">ESPN</li>
+            <center> <iframe width="900" height="400" src="//espn.go.com/espnw/athletes-life/article/9934629/espnw-roller-derby-enjoying-renaissance-heading-women%E2%80%99s-flat-track-derby-association-championships
+ " frameborder="0"></iframe></center>
+          </ul>
+        </div>
+
+    <div class="large-12 small-12 columns">
+          <ul class="pricing-table" style="background-color: #fff;">
+           <li class="bullet-item">Fox News</li>
+            <center> <iframe width="900" height="400" src="//www.fox2detroit.com/news/local-news/39526695-story" frameborder="0"></iframe></center>
+
+          </ul>
+        </div>
+        </div>
+
+<div class="row">
+<div class="large-12 columns">
+<div class="landing">
+<hr>
+  <img src="images/coaching.jpg">  
+  </div>
+
+<hr>
+
+    <div class ="panel product full-height">
+     <p class="detail" style="color: #808285;">Sponsors <span class="alt"></span> </p>
+
+        <div class="large-4 small-12 columns">
+           <ul class="pricing-table" style="background-color: #fff max-height: 200px;">
+
+            <center><img src="images/logo.png" style="max-height: 225px background-color: #fff;"></center>
+
+            </li>
+            <li class="price">FASTER SKATES</li>
+          </ul>
+        </div>
+
+                        <div class="large-4 small-12 columns">
+           <ul class="pricing-table" style="background-color: #fff;">
+            <center><img src="images/s1.png"></center>
+            </li>
+            <li class="price">S1</li>
+          </ul>
+        </div>
+
+                <div class="large-4 small-12 columns">
+           <ul class="pricing-table" style="background-color: #fff;">
+            <center><img src="images/pivotstar.png"></center>
+            </li>
+            <li class="price">PIVOTSTAR</li>
+             
+          </ul>
+        </div>
+</div>
+<hr>
+
+    <?php
+include('inc/footer.php'); ?>
+</div>
+
