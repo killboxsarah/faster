eea8b1ac0d0f1199dbfb6e6d5106234ec0f04c87
killboxsarah <killbox.sarah@gmail.com>
2015-12-21 14:29:54 -0600

    updating box set promotion page

--- box.php
+++ box.php
@@ -4,62 +4,93 @@ include('inc/header.php');
 $section = "special";
 ?>
 
-
-<!-- <div class="landing">
-  <img src="images/59_banner.jpg">  
-</div> -->
-
 <div class="panel product full-height" style="background-color: #eeeeee;">
  <p class="detail" style="color:#808285;" >BOX SET <span class="name" style="color:#ed1c24;">PROMOTION</span> </p>
 
-    <div class="large-6 small-12 columns">
-          <ul class="panel frame pricing-table left-side john">
-             <center><img src="images/boxs.jpg" alt="Multi Component" style= "max-height:600px"></a></center>
-            <li class="price" style="font-size:1.1em; text-aligh:center;background-color: #e3e3e3;font-size: 1.4em;padding-bottom: 0px;padding-top: 3%;">$205</li>
-            </ul>
-          </div>
+    <div class="large-4 columns">
+      <ul class="panel frame pricing-table left-side john">
+      <img src="img/products/dl59_wood.jpg" alt="Multi Component" style= "max-height:600px"></a>
+       </ul>
+    </div>
+    <div class="large-4 columns">
+    <ul class="panel frame pricing-table left-side john">
+      <img src="img/products/dr59tire_wood.jpg" alt="Multi Component" style= "max-height:600px"></a>
+      </ul>
+    </div>
 
-   <!--        <div class = "large-6 small-12 columns">
-          <ul class="panel frame pricing-table left-side">
-            </ul>
-          </div> -->
-              <div class="large-6 columns">
+              <div class="large-4 columns">
                 <ul class="pricing-table right-side">
                   <li class="title">
                       <div id="product-description" class="rte" itemprop="description">
                   <h3>59's Box Set</h3>
                       </div>
-               <!--    </li>
-                  <li class="price">$205 USD
-                  </li> -->
-                  <li class="description">Demilunes are perfect for a secure feel on slicker floors, without giving up the speed factor, and without the guesswork of 'pusher wheels'. The 90A gives you the grip when you need it, while the 94A does away with unnecessary fatigue from overly sticky wheels. 90 | 94a
-                  <br><br>
-                  Demirojo allows speed and agility to play the same game. The 92A allows you to control your edges, and the 96A provides sweet break and slide making it the perfect wheel for high speed footwork across a variety of surfaces. 92 | 96a
-                  <br><br>
-                  Set of 8 Wheels <br>
+                  </li>
+
+                  <li class="description">The beveled profile of our 59mm wheels puts the breaks on a slide for tight edges and direction changes.<br><br>
+                  Demirojo Red 92|96a <br>
+                  Demilune Purple 90|94a <br><br>
+                  Set of 8 Wheels + Set of 8 Tires<br>
                   Contact Patch: 34.5mm <br>
                   Total Width: 38.5mm <br>
                   Profile: 59 Bevel <br>
-                  <br>
-                  <br>
+                    <br>
                   <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="T5A6W4XJEZZ36">
-<input type="hidden" name="on0" value="Combo">Combo Options</td></tr><tr><td><select name="os0">
-  <option value="Complete Demirojo with Demilune Tires">Complete Demirojo with Demilune Tires $205.00 USD</option>
-  <option value="Complete Demilune with Demirojo Tires">Complete Demilune with Demirojo Tires $205.00 USD</option>
-</select> 
-<input type="hidden" name="currency_code" value="USD">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="hosted_button_id" value="T5A6W4XJEZZ36">
+                    <input type="hidden" name="on0" value="Combo">Combo Options</td></tr><tr><td><select name="os0">
+                      <option value="Complete Demirojo with Demilune Tires">Complete Demirojo with Demilune Tires $200.00 USD</option>
+                      <option value="Complete Demilune with Demirojo Tires">Complete Demilune with Demirojo Tires $200.00 USD</option>
+                    </select> 
+                    <input type="hidden" name="currency_code" value="USD">
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+                    </li>
+               </div>      
 
-</li>
-                </ul>           
-            </div>      
-      
 
-  </div>
+ <div class="large-4 columns">
+      <ul class="panel frame pricing-table left-side john">
+       <img src="img/products/naturalfw_wood.jpg" alt="Multi Component" style= "max-height:600px"></a>
+       </ul>
+       </ul>
+    </div>
+    <div class="large-4 columns">
+    <ul class="panel frame pricing-table left-side john">
+      <img src="img/products/dl59tire_wood.jpg" alt="Multi Component" style= "max-height:600px"></a>
+      </ul>
+    </div>
+
+              <div class="large-4 columns">
+                <ul class="pricing-table right-side">
+                  <li class="title">
+                      <div id="product-description" class="rte" itemprop="description">
+                  <h3>Skinny Box Set</h3>
+                      </div>
+                  </li>
+
+                  <li class="description">The complex-curve lip on our Skinny FlyWheels provide maximum snap on a narrow wheel.<br><br>
+                  Natural 97a <br>
+                  Demilune Purple 90|94a <br><br>
+                  Set of 8 Wheels + Set of 8 Tires<br>
+                  Contact Patch: 37.5mm <br>
+                  Total Width: 38.5mm <br>
+                  Profile: Skinny Profile <br>
+                    <br>
+                  <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="hosted_button_id" value="T5A6W4XJEZZ36">
+                    <input type="hidden" name="on0" value="Combo">Combo Options</td></tr><tr><td><select name="os0">
+                      <option value="Complete Demirojo with Demilune Tires">Complete Natural with Demilune Tires $200.00 USD</option>
+                      <option value="Complete Demilune with Demirojo Tires">Complete Demilune with Natural Tires $200.00 USD</option>
+                    </select> 
+                    <input type="hidden" name="currency_code" value="USD">
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+                    </li>
+               </div>      
+    </div>
 
   <div class ="panel dealer">
  <p class="detail" style="color: #808285;">TRUE TO THE <span class="alt">ART</span> </p>
