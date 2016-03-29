d34eea6be428a254f8eb3587a02506590b2c23a9
killboxsarah <killbox.sarah@gmail.com>
2016-03-29 15:09:39 -0400

    updated to make single component flywheel page

--- inc/singleproducts.php
+++ inc/singleproducts.php
@@ -115,15 +115,18 @@ $singleproducts[103] = array(
         Total Width: 38.5mm<br>
         Durometer: 90a | 94a",
 
-        "paypal" => '<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" target"_top">
-        <input type="hidden" name="cmd" value="_s-xclick">
-        <input type="hidden" name="hosted_button_id" value="H6VD924LXCQS2">
-          <option value="Skinny (8)">Skinny (8) $125.00 USD</option>
-          <option value="59mm (8)">59mm (8) $125.00 USD</option>
-        <input type="hidden" name="currency_code" value="USD">
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-        </form>'
+        "paypal" => '
+
+
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+      <input type="hidden" name="cmd" value="_s-xclick">
+      <input type="hidden" name="hosted_button_id" value="HLJY2WEHHG9ES">
+        <option value="62mm Skinny">62mm Skinny $125.00 USD</option>
+      <input type="hidden" name="currency_code" value="USD">
+      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+      </form>'
+
 
         );
 
